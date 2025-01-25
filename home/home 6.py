9c8f869afc0578707956b4746842a1c4e7923ebc
Arturchik07 <arstanbekovartur202@gmail.com>
2025-01-26 01:43:58 +0600

    homew6

--- home/home 6.py
+++ home/home 6.py
@@ -1,30 +1,39 @@
 def bubble_sort(arr):
-    for i in range(len(arr)):
-        for j in range(len(arr) - i - 1):
-            if arr[j] > arr[j + 1]:  # Если элемент больше следующего, меняем их
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
+def binary_search(value, arr):
+    num1 = 0
+    num2 = len(arr)-1
+    num = False
+    pos = -1
+    while num1 <= num2:
+        center = (num1+num2)//2
+        if arr[center] == value:
+            num = True
+            pos = center
+            break
+        elif arr[center] < value:
+            num1 = center + 1
+        else:
+            num2 = center - 1
+    if num:
+        print(f"Элемент найден: {pos}")
+    else:
+        print("Элемент не найден")
 
-def binary_search(target, arr):
-    left = 0
-    right = len(arr) - 1
 
-    while left <= right:
-        mid = (left + right) // 2  # Находим середину
-        if arr[mid] == target:  # Элемент найден
-            print(f"Элемент {target} найден на позиции {mid}")
-            return
-        elif arr[mid] < target:  # Ищем в правой половине
-            left = mid + 1
-        else:  
-            right = mid - 1
+list1 = [5, 7, 2, 6, 3, 4, 1]
+print(f"Неотсортированный спс: {list1}")
 
-    print(f"Элемент {target} не найден")
+sorted_list = bubble_sort(list1)
+print(f"Отсортированный спс: {sorted_list}")
 
+list2 = [2, 11, 19, 22, 30, 31, 46, 52, 55, 69, 76, 79, 81, 87, 95]
 
-nums = [64, 34, 25, 12, 22, 11, 90]
-sorted_nums = bubble_sort(nums)
-print("Отсортированный список:", sorted_nums)
-
-binary_search(34, sorted_nums)
+value1 = 79
+binary_search(value1, list2)
